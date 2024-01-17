55afe7e88483b4e45d08bbccaab87ad17f65d0a6
approachcircle <osc-mcd@protonmail.com>
2024-01-17 12:06:47 +0000

    add error handling and auto-restart

--- src/main/java/net/approachcircle/server/Main.java
+++ src/main/java/net/approachcircle/server/Main.java
@@ -1,61 +1,31 @@
 package net.approachcircle.server;
 
-import com.corundumstudio.socketio.Configuration;
-import com.corundumstudio.socketio.SocketIOServer;
-
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        Configuration config = new Configuration();
-        config.setPort(1909);
-        SocketIOServer server = new SocketIOServer(config);
-        server.addConnectListener(client -> System.out.printf("client connected at %s%n", client.getRemoteAddress()));
-        server.addDisconnectListener(client -> {
-            System.out.printf("client disconnected at %s%n", client.getRemoteAddress());
-            String roomCode = RoomManager.getInstance().getRoomByClient(client);
+        final int iteration_limit = 5;
+        int iterations = 0;
+        for (;;) {
             try {
-                RoomManager.getInstance().leaveRoom(roomCode, client);
-            } catch (RoomException ignored) {} // room didn't exist when client disconnected, doesn't really matter though
-        });
-        server.addEventListener("create-game", void.class, ((client, data, ackSender) -> {
-            String code = generateRoomCode();
-            client.joinRoom(code);
-            RoomManager.getInstance().createRoom(code);
-            RoomManager.getInstance().joinRoom(code, client);
-            ackSender.sendAckData(code);
-        }));
-        server.addEventListener("join-game", String.class, ((client, roomCode, ackSender) -> {
-            try {
-                RoomManager.getInstance().joinRoom(roomCode, client);
-            } catch (RoomException e) {
-                ackSender.sendAckData(State.Error, e.getMessage());
-                return;
+                new Application().run();
+                break;
+            } catch (RuntimeException e) {
+                System.err.println("fatal error caught, stack trace below: ");
+                e.printStackTrace(System.err);
+                System.err.println("dumping all open rooms: ");
+                System.err.println(RoomManager.getInstance().dumpOpenRooms());
+                System.err.println("restarting...");
+                try (NotificationServer server = new NotificationServer()) {
+                    server.send(e.getMessage());
+                }
             }
-            client.joinRoom(roomCode);
-            ackSender.sendAckData(State.OK, "joined room!");
-        }));
-        server.addEventListener("leave-game", String.class, ((client, roomCode, ackSender) -> {
-            try {
-                RoomManager.getInstance().leaveRoom(roomCode, client);
-            } catch (RoomException e) {
-                ackSender.sendAckData(State.Error, e.getMessage());
-                return;
+            iterations++;
+            if (iterations >= iteration_limit) {
+                System.err.println("iteration limit reached. quitting");
+                try (NotificationServer server = new NotificationServer()) {
+                    server.send("iteration limit reached. quitting");
+                }
+                break;
             }
-            client.leaveRoom(roomCode);
-            ackSender.sendAckData(State.OK, "left room!");
-        }));
-        System.out.println("server started");
-        server.start();
-    }
-
-    public static String generateRoomCode() {
-        Random rng = new Random();
-        StringBuilder code = new StringBuilder();
-        for (int i = 0; i < 7; i++) {
-            char c = (char)rng.nextInt(97, 123);
-            code.append(c);
         }
-        return code.toString();
     }
 }
